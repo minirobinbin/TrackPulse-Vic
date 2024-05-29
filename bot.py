753cf0e4c5417de4c9021cf9c16d29349f17c244
XM9G <xm9g@outlook.com>
2024-05-29 10:23:29 +1000

    licence is more prominant

--- bot.py
+++ bot.py
@@ -1,3 +1,20 @@
+'''Melbourne Public Transport Discord Bot
+    Copyright (C) 2024  Billy Evans
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.'''
+
+
 from discord.ext import commands, tasks
 from discord import app_commands
 import discord
@@ -32,6 +49,11 @@ from utils.trainset import *
 from utils.trainlogger.stats import *
 from utils.trainlogger.ids import *
 
+
+print("""Melbourne Public Transport Discord Bot  Copyright (C) 2024  Billy Evans
+    This program comes with ABSOLUTELY NO WARRANTY.
+    This is free software, and you are welcome to redistribute it
+    under certain conditions""")
 
 file = open('utils\\stations.txt','r')
 stations_list = []
