466291e7f58cfb68526771dcc2b95f137739496e
XM9G <xm9g@outlook.com>
2024-05-29 10:40:58 +1000

    moved function into seperate file

--- bot.py
+++ bot.py
@@ -48,6 +48,8 @@ from utils.trainlogger.main import *
 from utils.trainset import *
 from utils.trainlogger.stats import *
 from utils.trainlogger.ids import *
+from utils.unixtime import *
+
 
 
 print("""Melbourne Public Transport Discord Bot  Copyright (C) 2024  Billy Evans
@@ -83,18 +85,6 @@ try:
     os.mkdir('utils/game/scores')
 except FileExistsError:
     pass    
-
-
-def convert_to_unix_time(date: datetime.datetime) -> str:
-    # Get the end date
-    end_date = date
-
-    # Get a tuple of the date attributes
-    date_tuple = (end_date.year, end_date.month, end_date.day, end_date.hour, end_date.minute, end_date.second)
-
-    # Convert to unix time
-    return f'<t:{int(time.mktime(datetime.datetime(*date_tuple).timetuple()))}:R>'
-
 
 # Group commands
 class CommandGroups(app_commands.Group):
---
+++ utils/unixtime.py
@@ -0,0 +1,9 @@
+def convert_to_unix_time(date: datetime.datetime) -> str:
+    # Get the end date
+    end_date = date
+
+    # Get a tuple of the date attributes
+    date_tuple = (end_date.year, end_date.month, end_date.day, end_date.hour, end_date.minute, end_date.second)
+
+    # Convert to unix time
+    return f'<t:{int(time.mktime(datetime.datetime(*date_tuple).timetuple()))}:R>'
