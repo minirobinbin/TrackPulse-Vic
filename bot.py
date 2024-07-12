e84a4a75408d45bb62c8920a9f3b2d21979b73fd
XM9G <xm9g@outlook.com>
2024-07-12 10:18:00 +1000

    help command

--- bot.py
+++ bot.py
@@ -183,6 +183,26 @@ async def task_loop():
         print("Rare checker not enabled!")
 
 
+# Help command
+@bot.tree.command(name='help', description='Run help if you want to know about a command')
+async def help(ctx):
+    async def helper():
+        generalCmds ="""</help:1261107050545549342> - Shows this command
+</stats profile:1240101357847838815> - View your profile with variouse stats across your logs and game wins"""
+        logCmds = """</logs add-train:1254387855820849154> - Add a train in Victoria you have been on, arguments: `line` - The line the train was on, `number` - The carrige number you went on (the full set will autofill), `date` - will autofill to today if empty, `start` - station you got on at, `end` - station you got off at, `traintype` - type of train, will autofill if train number entered.
+</logs add-sydney-train:1254387855820849154> - same as above but for trains in NSW
+</logs add-tram:1254387855820849154> - same as above but for trams in Melbourne
+</logs add-sydney-tram:1254387855820849154> - same as above but for light rail in Sydney
+
+</logs view:1254387855820849154> - view your logs
+</logs delete:1254387855820849154> - delete one of your logs, leave id blank to delete the last log from the selected mode. The id can be seen with </logs view:1254387855820849154>
+</logs stats:1254387855820849154> - view variouse stats and graphs from your logged trips."""
+        searchCmds = """</search train:1240101357847838814> - Input a carrige number to see info about it, such as it's type, next services, livery and more!
+</search departures:1240101357847838814> - View the next 10 departures for a station
+</search metro-line:1240101357847838814> - View disruptions for a Metro Trains line
+</search route:1240101357847838814> - View disruptions for a tram or bus route"""
+        await ctx.response.send_message(f"# Command help\n{generalCmds}\n## Log Commands\n{logCmds}\n## Search commands\n{searchCmds}")
+    asyncio.create_task(helper())
 
     
 
