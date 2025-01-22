37d39624885531043381762c1543913ab0c64be2
Comeng17 <177193320+Comeng17@users.noreply.github.com>
2025-01-22 15:00:15 +1100

    minor fix for /help

--- bot.py
+++ bot.py
@@ -649,7 +649,7 @@ Required:
     Route: which tram route you rode on. You have to choose one of the options.
 
 Optional:
-    Number: which carriage you rode. Examples include "856", "123", "2003", "6001". If you don't know, type "Unknown".
+    Number: which tram you rode. Examples include "856", "123", "2003", "6001". If you don't know, type "Unknown".
     Date: if the trip is a trip from the past, input the date here, otherwise, the current date will be added.
     Start: the starting stop of your trip. You can choose from the list or type your own.
     End: the ending stop of your trip. You can choose from the list or type your own.''',
