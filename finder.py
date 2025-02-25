4af28c33eddb08fb7178f45860563c4c28f17392
Comeng17 <177193320+Comeng17@users.noreply.github.com>
2025-02-26 08:52:44 +1100

    ok turns out main does need its own dictionary to work offline

--- finder.py
+++ finder.py
@@ -2,5 +2,3 @@ from utils.trainlogger.map.mapimage import CoordinateFinder, CoordinateCorrector
 
 finder = CoordinateFinder("utils/trainlogger/map/log_train_map.png")
 finder.run()
-else:
-    print("Syntax Error: Restart to try again")
