3ba8a66c03ed19bb5495c95c87f5323091036796
Comeng17 <177193320+Comeng17@users.noreply.github.com>
2025-02-25 21:31:46 +1100

    ill get it eventually

--- finder.py
+++ finder.py
@@ -1,6 +1,5 @@
 from utils.trainlogger.map.mapimage import CoordinateFinder, CoordinateCorrector
 
-answer = input("Type F for finder, C for corrector")
 finder = CoordinateFinder("utils/trainlogger/map/log_train_map.png")
 finder.run()
 else:
