749b7deb26359d77a7bde38bbf462340c5b7f8b9
Comeng17 <177193320+Comeng17@users.noreply.github.com>
2025-02-25 21:30:49 +1100

    fix

--- finder.py
+++ finder.py
@@ -1,8 +1,7 @@
 from utils.trainlogger.map.mapimage import CoordinateFinder, CoordinateCorrector
 
 answer = input("Type F for finder, C for corrector")
-if answer == "F":
-    finder = CoordinateFinder("utils/trainlogger/map/log_train_map.png")
-    finder.run()
+finder = CoordinateFinder("utils/trainlogger/map/log_train_map.png")
+finder.run()
 else:
     print("Syntax Error: Restart to try again")
