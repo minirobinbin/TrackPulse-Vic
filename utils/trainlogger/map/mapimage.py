c7b530c9aa5a3f071d72aab68c024f1c01eaedaf
Comeng17 <177193320+Comeng17@users.noreply.github.com>
2025-03-11 18:32:24 +1100

    made margins normal size

--- utils/trainlogger/map/mapimage.py
+++ utils/trainlogger/map/mapimage.py
@@ -5,8 +5,9 @@ from matplotlib.pylab import f
 import math
 
 dpi = 32/96
-padding = 1.5
-margin_watermark = (1 - (1 / padding)) / 4
+padding = 1.1
+vertical_padding = 1.5
+margin_watermark = (1 - (1 / vertical_padding)) / 2
 
 def compress(image: Image):
     print("Compressing Image")
@@ -128,7 +129,7 @@ class MapImageHandler:
             if bbox:
                 print('Image Cropped')
                 image_cropped = image.crop(bbox)
-                image_padding = Image.new(image_cropped.mode, (round(image_cropped.size[0] * padding), round(image_cropped.size[1] * padding)), (255,255,255))
+                image_padding = Image.new(image_cropped.mode, (round(image_cropped.size[0] * padding), round(image_cropped.size[1] * vertical_padding)), (255,255,255))
                 image_padding.paste(image_cropped, (round(image_cropped.size[0] * (padding - 1) / 2), round(image_cropped.size[1] * (padding - 1) / 2)))
                 return image_padding
             else:
