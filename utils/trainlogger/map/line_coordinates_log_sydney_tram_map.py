d3551c158dd37f633d30ab43f828d55fe82fabcd
Comeng17 <177193320+Comeng17@users.noreply.github.com>
2025-03-11 16:52:17 +1100

    fixed overlay

--- utils/trainlogger/map/line_coordinates_log_sydney_tram_map.py
+++ utils/trainlogger/map/line_coordinates_log_sydney_tram_map.py
@@ -7,6 +7,68 @@ line_coordinates = {
             (9650 + x_offset, 4350 + y_offset, 9800 + x_offset, 4500 + y_offset), # Central Coords
             (9550 + x_offset, 4500 + y_offset, 9750 + x_offset, 4600 + y_offset), # Central Coords
             (9300 + x_offset, 4500 + y_offset, 9550 + x_offset, 4650 + y_offset), # Central Coords
-        ]
+        ],
+        ("Paddy's Markets", 'Capitol Square'): [
+            (8900 + x_offset, 3750 + y_offset, 8900 + x_offset, 3850 + y_offset),
+            (8900 + x_offset, 3800 + y_offset, 9750 + x_offset, 3850 + y_offset),
+            (9700 + x_offset, 3800 + y_offset, 9800 + x_offset, 4000 + y_offset),
+        ],
+        ('Exhibition Centre', "Paddy's Markets"): [
+            (8350 + x_offset, 3750 + y_offset, 8950 + x_offset, 3850 + y_offset),
+        ],
+        ('Convention', 'Exhibition Centre'): [
+            (7750 + x_offset, 3750 + y_offset, 8400 + x_offset, 3850 + y_offset),
+        ],
+        ('Pyrmont Bay', 'Convention'): [
+            (7150 + x_offset, 3750 + y_offset, 7800 + x_offset, 3850 + y_offset),
+        ],
+        ('John Street Square', 'Pyrmont Bay'): [
+            (6550 + x_offset, 3750 + y_offset, 7200 + x_offset, 3850 + y_offset),
+        ],
+        ('Fish Market', 'John Street Square'): [
+            (6000 + x_offset, 3750 + y_offset, 6600 + x_offset, 3850 + y_offset),
+        ],
+        ('Wentworth Park', 'Fish Market'): [
+            (5400 + x_offset, 3750 + y_offset, 6050 + x_offset, 3800 + y_offset),
+        ],
+        ('Glebe', 'Wentworth Park'): [
+            (4900 + x_offset, 3750 + y_offset, 5450 + x_offset, 3850 + y_offset),
+        ],
+        ('Jubilee Park', 'Glebe'): [
+            (4450 + x_offset, 3750 + y_offset, 4950 + x_offset, 3850 + y_offset)
+        ],
+        ('Rozelle Bay', 'Jubilee Park'): [
+            (4000 + x_offset, 3750 + y_offset, 4500 + x_offset, 3850 + y_offset),
+        ],
+        ('Lilyfield', 'Rozelle Bay'): [
+            (3550 + x_offset, 3750 + y_offset, 4050 + x_offset, 3850 + y_offset),
+        ],
+        ('Leichhardt North', 'Lilyfield'): [
+
+        ],
+        ('Hawthorne', 'Leichhardt North'): [
+
+        ],
+        ('Marion', 'Hawthorne'): [
+
+        ],
+        ('Taveners Hill', 'Marion'): [
+
+        ],
+        ('Lewisham West', 'Taveners Hill'): [
+
+        ],
+        ('Waratah Mills', 'Lewisham West'): [
+
+        ],
+        ('Arlington', 'Waratah Mills'): [
+
+        ],
+        ('Dulwich Grove', 'Arlington'): [
+
+        ],
+        ('Dulwich Hill', 'Dulwich Grove'): [
+
+        ],
     }
 }
