56bd168dcc47676acdcc6f3b8ace855a4b6ce04d
Billy <xm9G@outlook.com>
2025-08-24 18:16:12 +1000

    fixed image acceper for guesser

--- utils/game/imageadder.py
+++ utils/game/imageadder.py
@@ -3,6 +3,7 @@ import sqlite3
 import os
 
 from photosubmissions.manager import getUserID
+from utils.trainlogger.map.uploadimage import uploadImage
 from utils.vicrailphotosapi.accepter import convertToWEBP
 
 
@@ -16,23 +17,12 @@ async def acceptGuesserPhoto(id: int, station: str, difficulty: str, mode: str, 
     conn.close()
     
     image_filename = rows[0][2]
-    guesserPath = f'assets/guesser/images/'
     imagePath = f'photosubmissions/photos/{image_filename}'
-    
-    if not os.path.exists(guesserPath):
-        os.makedirs(guesserPath)
-    
-    # move image
-    os.rename(imagePath, guesserPath + image_filename)
-    
-    convertToWEBP(
-        input_path=guesserPath + image_filename,
-        output_path=guesserPath + os.path.splitext(image_filename)[0] + '.webp'
-    )
-    
-    
+        
+    print('Uploading image to imgbb')
+    url = uploadImage(imagePath, name=os.path.splitext(image_filename)[0])
     
     print(f'adding to {mode}.csv: {id}, {station}, {difficulty}')
     with open(f'utils/game/images/{mode}.csv', 'a', newline='') as file:
         writer = csv.writer(file)
-        writer.writerow([f"https://flying-thrush-early.ngrok-free.app/guesser/{image_filename.split('.')[0]}.webp", station, difficulty, username])
+        writer.writerow([url, station, difficulty, username])
