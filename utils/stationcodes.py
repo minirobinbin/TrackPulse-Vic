346f0a1d6458cdf4c99b8c3e8ca34343dea6a13e
NebulaFire <nebulablackesthole@gmail.com>
2026-06-01 20:13:35 +1000

    More Station codes!

--- utils/stationcodes.py
+++ utils/stationcodes.py
@@ -1,18 +1,36 @@
 station_codes = {
+    # Camperdown Was the Most recent station (following vicsig and sorting by name)
+    
     # Clifton Hill Station Codes
+    "BEL":  "Bell",
     "ALP":	"Alphington",
     # Burnley Station Codes
+    "BLY":  "Burnley",
     "AUB":	"Auburn",
+    "CAM":  "Camberwell",
     "ASH":	"Ashburton",
     "UNN":	"Union",
-    "ALM": "Alamein",
+    "BOX":  "Box Hill",
+    "BBN":  "Blackburn",
+    "BAY":  "Bayswater",
+    "BOR":	"Boronia",
+    "BEG":  "Belgrave",
+    "BWD":  "Burwood",
+    "ALM":  "Alamein",
     # Cran/Pak Station Codes
+    "BEW":  "Berwick",
+    "BFD":  "Beaconsfield",
     "EPH":	"East Pakenham",
     # Frankston/Stony Point Station Codes
     "ARM":	"Armadale",
+    "BEN":  "Bentleigh",
     "ASP":	"Aspendale",
+    "BON":  "Bonbeach",
+    "BXR":  "Baxter",
+    "BIT":  "Bittern",
     # Sandringham Station Codes
     "BCV":  "Balaclava",
+    "BBH":  "Brighton Beach",
     # Werribee/Williamstown Station Codes
     "ALT":	"Altona",
     "ACF":	"Aircraft",
@@ -21,8 +39,11 @@ station_codes = {
     "ALB":	"Albion",
     # Craigieburn Station Codes
     "ASV":	"Ascot Vale",
+    "BMS":  "Broadmeadows",
     # Upfield Station Codes
+    "BWK":  "Brunswick",
     "ASY":	"Anstey",
+    "BAT":  "Batman",
     # Misc/VLine Station Codes
     "ALY":	"Albury",
     "ART":	"Ararat",
@@ -36,6 +57,28 @@ station_codes = {
     "TLN":	"Cobblebank",
     "BNV":  "Ballan",
     "BAL":  "Ballarat",
+    "BBL":  "Bank Box Loop", # What even is this? But its here anyways
+    "BAW":  "Barnawartha",
+    "BWP":  "Barwon Park Loop", # Again, is this real (vicsig says it is), no matter what, its here anyway
+    "BEA":  "Bena",
+    "BNL":  "Benalla",
+    "BDG":  "Bendigo",
+    "GRS":  "Bombardier Siding", # Why do these station codes exist
+    "ACI":  "Bonlac Foods Limited Siding", # I'm going to stop writing these reduntant coding commments now :D
+    "BRC":  "Boorcan",
+    "BSR":  "Bowser Loop",
+    "SBL":  "Box Hill", # Box Hill (underground) what are you on vicsig, anyway its there if you want to use it to log box hill
+    "BSK":  "Bradshaw",
+    "BRF":  "Broadford",
+    "MBL":  "Bungaree East End", #....
+    "BGL":  "Bungaree Loop",
+    "BGN":  "Bungaree West End",  # Too many Bungaree station codes
+    "BYP":  "Bunyip",
+    "BLS":  "Burnley Stabling Sidings",
+    "BBT":  "Burrumbeet",
+    "SBU":  "Burwood", # Another (underground) station on vicsig
+    "CPV":  "Calder Park Sidings",
+    "CPD":  "Camperdown",
     
     # Inner City stations With too many lines
     "FSS":	"Flinders Street",
@@ -53,7 +96,7 @@ station_codes = {
     "RMD":	"Richmond",
     
     # Added early for specific people
-    "BOR":	"Boronia",
+    
     "FTG":	"Ferntree Gully",
     "SYN":	"Syndal",
 
