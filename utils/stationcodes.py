cd1dee9d20f9e1220ec74c2abb7610bfb0aecdc0
NebulaFire <nebulablackesthole@gmail.com>
2026-06-01 19:21:18 +1000

    Added more station codes
    will definitely add more in a bit, just reorganised and started adding

--- utils/stationcodes.py
+++ utils/stationcodes.py
@@ -1,32 +1,57 @@
 station_codes = {
-    "ALB":	"Albion",
-    "ACF":	"Aircraft",
-    "ALY":	"Albury",
+    # Clifton Hill Station Codes
     "ALP":	"Alphington",
-    "ALT":	"Altona",
-    "ALM":	"Alamein",
-    "ARM":	"Armadale",
-    "ADR":	"Ardeer",
-    "ARN":	"Arden",
-    "ART":	"Ararat",
-    "ASH":	"Ashburton",
-    "ASP":	"Aspendale",
-    "ASV":	"Ascot Vale",
-    "ASY":	"Anstey",
+    # Burnley Station Codes
     "AUB":	"Auburn",
-    "AZC":	"Anzac",
-    "BMH":	"Bacchus Marsh",
+    "ASH":	"Ashburton",
+    "UNN":	"Union",
+    "ALM": "Alamein",
+    # Cran/Pak Station Codes
+    "EPH":	"East Pakenham",
+    # Frankston/Stony Point Station Codes
+    "ARM":	"Armadale",
+    "ASP":	"Aspendale",
+    # Sandringham Station Codes
+    "BCV":  "Balaclava",
+    # Werribee/Williamstown Station Codes
+    "ALT":	"Altona",
+    "ACF":	"Aircraft",
 
-    # Should be in now
+    # Sunbury Station Codes
+    "ALB":	"Albion",
+    # Craigieburn Station Codes
+    "ASV":	"Ascot Vale",
+    # Upfield Station Codes
+    "ASY":	"Anstey",
+    # Misc/VLine Station Codes
+    "ALY":	"Albury",
+    "ART":	"Ararat",
+    "ADR":	"Ardeer",
+    "BMH":  "Bacchus Marsh",
+    "BRD":  "Bairnsdale",
+    "TNT":	"Tarneit",
+    "WPD":	"Waurn Ponds",
+    "WVL":	"Wyndham Vale",
+    "CPS":	"Caroline Springs",
+    "TLN":	"Cobblebank",
+    "BNV":  "Ballan",
+    "BAL":  "Ballarat",
+    
+    # Inner City stations With too many lines
     "FSS":	"Flinders Street",
     "SSS":	"Southern Cross",
     "SCS":	"Southern Cross", # Not official that may be used by one or 2 people
     "FGS":	"Flagstaff",
     "MCE":	"Melbourne Central",
     "PAR":	"Parliament",
+    "ARN":	"Arden",
+    "PKV":	"Parkville",
+    "STL":	"State Library",
+    "THL":	"Town Hall",
+    "AZC":	"Anzac",
     "NME":	"North Melbourne",
     "RMD":	"Richmond",
-
+    
     # Added early for specific people
     "BOR":	"Boronia",
     "FTG":	"Ferntree Gully",
@@ -34,16 +59,7 @@ station_codes = {
 
     # Three Letter Stations
     "MOE":	"Moe",
-
-    # Aren't on VicSig
-    "PKV":	"Parkville",
-    "STL":	"State Library",
-    "THL":	"Town Hall",
-    "UNN":	"Union",
-    "EPH":	"East Pakenham",
-    "WPD":	"Waurn Ponds",
-    "TNT":	"Tarneit",
-    "WVL":	"Wyndham Vale",
-    "CPS":	"Caroline Springs",
-    "TLN":	"Cobblebank",
+    
+    
+    
 }
